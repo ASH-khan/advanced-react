180c946512a1da4ba733c3ba0f7fc92ea6251ff1
jdpenuliar <jaydz.penuliar@gmail.com>
2017-03-31 14:38:09 -0700

    asdf

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -12,13 +12,23 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
+import styled from 'styled-components';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
+    const Title = styled.h1`
+      font-size: 1.5em;
+      text-align: center;
+      color: palevioletred;
+    `;
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <div>
+        <h1>
+          <FormattedMessage {...messages.header} />
+        </h1>
+        <Title>asdf</Title> 
+        <button type="button" className="btn btn-primary">Primary</button>
+      </div>
     );
   }
 }
